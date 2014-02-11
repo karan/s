9ff34d70f9d3878cb670ea91a65d021d12a91556
Karan Goel <karan@goel.im>
2014-02-11 12:13:40 -0800

    move controller to directive

--- public/javascripts/sController.js
+++ public/javascripts/sController.js
@@ -1,34 +1,27 @@
-var sApp = angular.module("sApp", ["ngAnimate"]);
-
-sApp.controller("sCtrl", ["$scope", "$http", function($scope, $http) {
-    $scope.long_url = "";
-    $scope.short_url = "";
-
-    $scope.shorten = function (long_url) {
-        console.log(long_url);
-        $http.post("/new", {'long_url': long_url})
-        .success(function(data) {
-            $scope.short_url = data["short_url"];
-            console.log($scope.short_url);
-            $scope.long_url = $scope.short_url;
-        })
-        .error(function(data) {
-            alert("Something went wrong. Try again!");
-        });
-    }
-}]);
+var sApp = angular.module("sApp", []);
 
 sApp.directive("toggleBtn", function () {
     return {
         restrict: 'A',
+        controller: function($scope, $http) {
+            $scope.long_url = "";
+            $scope.short_url = "";
+
+            $scope.shorten = function (long_url) {
+                console.log(long_url);
+                $http.post("/new", {'long_url': long_url})
+                .success(function(data) {
+                    $scope.short_url = data["short_url"];
+                    console.log($scope.short_url);
+                })
+                .error(function(data) {
+                    alert("Something went wrong. Try again!");
+                });
+            }
+        },
         link: function (scope, elem, attrs) {
             elem.bind("click", function () {
-                console.log(elem + ' clicked');
-                if(elem.val() == "Shorten") {
-                    elem.val("Copy");
-                } else {
-                    elem.val("Shorten");
-                }
+                scope.shorten(scope.long_url);
             });
         }
     }
